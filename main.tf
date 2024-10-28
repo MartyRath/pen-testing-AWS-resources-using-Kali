9f11001a34ba2221602254576a2f6bb4e9d1dfeb
Marty Rath <martydavidger@gmail.com>
2024-10-28 09:23:07 +0000

    Cleaned up, added security group, instance, vpc module

--- main.tf
+++ main.tf
@@ -1,16 +1,57 @@
-# Provider configuration
-provider "aws" {
-  region = "us-east-1"  # Change to your preferred region
-}
+# Creates VPC, subnets, custom route tables, internet gateway, Network ACL 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-# VPC
-resource "aws_vpc" "pentest_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  name = "pentest_VPC"
+  cidr = "10.0.0.0/16"
+
+  # Availability zones
+  azs = ["us-east-1a"]
+
+  # Creating public subnet
+  public_subnets       = ["10.0.101.0/24"]
+  public_subnet_names  = ["pentest_public_subnet"]
+
+  # Auto-assigns public IPv4 address for instances launched in public subnets
+  map_public_ip_on_launch = true
+
+  # Disable creation of default security group
+  manage_default_security_group = false
 
   tags = {
-    Name = "pentest-vpc"
+    Terraform   = "true"
     Environment = "testing"
   }
+}
+
+resource "aws_instance" "vulnerable_ec2" {
+  ami                    = "ami-005fc0f236362e99f" # Ubuntu 22.04
+  instance_type          = "t2.nano"
+  subnet_id              = module.vpc.public_subnets[0] # Creates instance in first available public VPC subnet
+  vpc_security_group_ids = [aws_security_group.vulnerable_sg.id]
+  key_name               = "new-key"        # Key name for ssh
+
+  # Running script to install apache, mysql
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2 mysql-server
+              systemctl start apache2
+              systemctl enable apache2
+              
+              # Configure MySQL to accept remote connections (vulnerable configuration)
+              sed -i 's/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+              systemctl restart mysql
+              
+              # Create a test webpage
+              echo "<html><body><h1>Vulnerable Test Server</h1></body></html>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "vulnerable_ec2"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.vulnerable_ec2.public_ip
 }
---
+++ security-group.tf
@@ -0,0 +1,52 @@
+# Security Group 
+# Allows SSH, HTTP, HTTPS, MySQL from any IP address 
+resource "aws_security_group" "vulnerable_sg" {
+  name        = "vulnerable-sg"
+  description = "Intentionally vulnerable security group for testing"
+  vpc_id      = module.vpc.vpc_id
+
+  # Allows SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allows HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allows HTTPS
+  # ingress {
+  #   from_port   = 443
+  #   to_port     = 443
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  # Allows MySQL
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allows all outbound traffic from anywhere, all protocols
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "vulnerable-sg"
+    Environment = "testing"
+  }
+}
